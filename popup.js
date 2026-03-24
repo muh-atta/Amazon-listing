4303eed0d7d4e0c0cadbbe598d4640ff618496e9
muh-atta <muh.atta@gmail.com>
2026-03-24 12:57:30 +0500

    Detect if the current tab is an Amazon page

--- popup.js
+++ popup.js
@@ -1,1 +1,25 @@
-console.log('Hello from popup.js!')
+
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  const tab = tabs[0];
+  const statusDiv = document.getElementById("status");
+
+  if (!tab || !tab.url) {
+    statusDiv.textContent = "No active tab found";
+    statusDiv.style.backgroundColor = "gray";
+    return;
+  }
+
+  const url = new URL(tab.url);
+  const isAmazon = url.hostname.includes("amazon.");
+  const urlParams = url.searchParams;
+  const isListing = url.pathname === "/s" && urlParams.has("k");
+  const isHomepage = url.pathname === "/" || url.pathname === "/ref=nav_logo";
+
+  if ( (isAmazon && (isListing || isHomepage))) {
+    statusDiv.textContent = "amazon.com";
+    statusDiv.style.backgroundColor = "green";
+  } else {
+    statusDiv.textContent = "Visit Amazon listing page";
+    statusDiv.style.backgroundColor = "red";
+  }
+});
