cf0bad7da4952a83ecaaff616d465682bef34762
muh-atta <muh.atta@gmail.com>
2026-03-24 13:59:22 +0500

    Adding toggle button in the popup

--- popup.js
+++ popup.js
@@ -1,4 +1,17 @@
+const statusDiv = document.getElementById("status");
+const toggleBtn = document.getElementById("toggleBtn");
 
+let isStarted = false;
+
+// Toggle button click handler
+toggleBtn.addEventListener("click", () => {
+  isStarted = !isStarted; 
+  toggleBtn.textContent = isStarted ? "Stop" : "Start";
+
+  console.log(isStarted ? "Started" : "Stopped");
+})
+
+// Logic to verify active tab is amazon or not
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   const tab = tabs[0];
   const statusDiv = document.getElementById("status");
